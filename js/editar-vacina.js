c414ef4decaf2be0449888eb624818ced7fc5b26
Gabriel Perino Feitosa <50030104+gabrielfeitosa00@users.noreply.github.com>
2022-12-03 15:30:32 -0300

    feat: get and populate vaccine

--- js/editar-vacina.js
+++ js/editar-vacina.js
@@ -1,4 +1,4 @@
-import mockVaccine from "./mock-data.js";
+import { getVaccineById } from "./crud-vacina.js";
 window.addEventListener("load", () => {
   const deleteButton = document.querySelector("#excluir");
 
@@ -17,6 +17,9 @@ window.addEventListener("load", () => {
   });
   loadSingleVaccine();
 });
+let vaccineId = null
+let file = null
+let filePath = null
 const openModal = () => {
   const modal = document.querySelector(".modal");
 
@@ -33,31 +36,41 @@ const closeModalWindow = (evt) => {
   if (evt.target === modal) modal.style.display = "none";
 };
 
-const loadSingleVaccine = () => {
+const loadSingleVaccine = async () => {
   const queryString = new URLSearchParams(document.location.search);
-  const id = queryString.get("id");
 
-  if (!id) {
+  vaccineId = queryString.get("id")
+  const vaccine = await getVaccineById(vaccineId)
+  if (!vaccineId||!vaccine) {
     window.location.assign("/home.html");
   }
 
-  const vaccine = mockVaccine.filter((item) => item.id === id)[0];
+ 
   console.log(vaccine);
   populateInputs(vaccine);
-  if (!vaccine) {
-    window.location.assign("/home.html");
-  }
+
 };
 
 const populateInputs = (vaccine) => {
   const vaccineDate = document.querySelector("#vaccine-date");
-  vaccineDate.value = vaccine.data;
+  vaccineDate.value = vaccine.data().data;
   const vaccineTitle = document.querySelector("#vaccine-title");
-  vaccineTitle.value = vaccine.name;
-  const dose = document.querySelector(`#${vaccine.dose}`);
+  vaccineTitle.value = vaccine.data().name;
+  const dose = document.querySelector(`#${vaccine.data().dose}`);
   dose.checked = true;
-  if (!!vaccine["proxData"]) {
+  filePath = vaccine.data().filePath
+  const img = document.querySelector("#comprovante")
+  img.src = vaccine.data().url
+  img.style.display = "block"
+  if (!!vaccine.data().proxData) {
     const vaccineDateProx = document.querySelector("#proxData");
-    vaccineDateProx.value = vaccine["proxData"];
+    vaccineDateProx.value = vaccine.data().proxData;
   }
 };
+
+const imgPreview = (event) =>{
+  file = event.target.files[0]
+  const img = document.querySelector("#comprovante")
+  img.src = URL.createObjectURL(file)
+  img.style.display = "block"
+}
